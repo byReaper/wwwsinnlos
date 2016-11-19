191bc23752c16bbb177b8ab3e08a43502acbbfc1
Leon <byreaper@gmx.de>
2016-11-19 11:48:44 +0100

    aa

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost"; // Host (meistens unverändert lassen/localhost)
 $user = "root"; // Benutzer der Datenbank (meistens root)
-$password = "loofti88"; // Passwort für die Datenbank.
+$password = "1234"; // Passwort für die Datenbank.
 $database = "site"; // Datenbank, welche benutzt werden soll.
 $db = mysqli_connect($host, $user, $password, $database); // Hier werden die Variablen eingetragen, um eine Verbindung zur Datenbank aufzubauen.
 ?>
